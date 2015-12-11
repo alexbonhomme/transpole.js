a12dce0317316c663c29ec70fdaf96382162d9c6
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2015-12-11 10:58:31 +0100

    Implements options argument to give access to proxy url configuration

--- transpole.js
+++ transpole.js
@@ -1,10 +1,32 @@
-/*global XMLHttpRequest*/
+/*jslint browser:true*/
 var transpole = (function () {
     'use strict';
 
     var API_SUBSIDIARY_ID = '9d16ee6b-15a1-4963-bdeb-54d15dc27365',
-        API_PROXY_BASE = 'http://localhost:8000/';
 
+        options = {
+            apiProxyBase: 'http://localhost:8000/'
+        };
+
+    /**
+     * Set customs options into options object.
+     * @param  {Object} customOptions [description]
+     */
+    function handleOptions(customOptions) {
+        var key;
+
+        for (key in customOptions) {
+            if (customOptions.hasOwnProperty(key) && options.hasOwnProperty(key)) {
+                options[key] = customOptions[key];
+            }
+        }
+    }
+
+    /**
+     * Format params object to url query args string.
+     * @param  {Object} params [description]
+     * @return {String}        [description]
+     */
     function formatParams(params) {
         var key,
             query = [];
@@ -20,8 +42,8 @@ var transpole = (function () {
 
     /**
      *
-     * @param  {String} path    [description]
-     * @param  {Object} params  [description]
+     * @param  {String}   path    [description]
+     * @param  {Object}   params  [description]
      * @param  {Function} success [description]
      * @param  {Function} error   [description]
      */
@@ -31,7 +53,7 @@ var transpole = (function () {
             query;
 
         // build url
-        url = API_PROXY_BASE + path + '/' + API_SUBSIDIARY_ID;
+        url = options.apiProxyBase + path + '/' + API_SUBSIDIARY_ID;
 
         // build query params
         query = formatParams(params);
@@ -67,19 +89,29 @@ var transpole = (function () {
         return 'transpole:StopArea:' + stopName;
     }
 
-    return {
-        getNext: function (lineName, stopName, direction) {
-            var params = {
-                lineId: lineId(lineName),
-                stopId: stopId(stopName),
-                wayId: direction
-            };
+    /**
+     * Publics
+     */
 
-            return {
-                then: function (success, error) {
-                    request('/nextSchedule', params, success, error);
-                }
-            };
-        }
+    function getNext(lineName, stopName, direction) {
+        var params = {
+            lineId: lineId(lineName),
+            stopId: stopId(stopName),
+            wayId: direction
+        };
+
+        return {
+            then: function (success, error) {
+                request('/nextSchedule', params, success, error);
+            }
+        };
+    }
+
+    return function (options) {
+        handleOptions(options);
+
+        return {
+            getNext: getNext
+        };
     };
 }());
